a9742bdd9423cd7a51b6cd1eb192fd16d521c748
Numo <numofran6@gmail.com>
2025-03-29 11:12:31 +0000

    Add Generate Credit page

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -4,7 +4,6 @@ import Header from './Header';
 import Partners from './Partners';
 import Services from './Services';
 import Trustworthiness from './Trustworthiness';
-import './index.css';
 
 function Home() {
 	return (
@@ -15,14 +14,6 @@ function Home() {
 			<Trustworthiness />
 			<Services />
 			<Footer />
-
-			<div className="pop-up fixed inset-0 bg-white/30">
-				<main className="flex items-center justify-center h-full w-full">
-					<div className="border border-[#29ad8d] rounded-[20px] p-8">
-						<h2>Calculate Your Score</h2>
-					</div>
-				</main>
-			</div>
 		</>
 	);
 }
